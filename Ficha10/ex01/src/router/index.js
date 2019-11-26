0ccfdac6cab5ee1bab9f9c47e467184c34d36858
Nuno <9170511@esmad.ipp.pt>
2019-11-26 12:38:00 +0000

    ficha10 ex01

---
+++ Ficha10/ex01/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Contacts from "../views/Contacts.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About
+  },
+  {
+    path: "/contacts",
+    name: "contacts",
+    component: Contacts
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
